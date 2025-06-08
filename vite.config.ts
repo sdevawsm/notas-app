1892bfd371f622ff817dc9c931756d35d741cfc7
dionemourao <silvadevbr@gmail.com>
2025-06-08 12:12:45 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Base relativa - funciona em ambos
   build: {
-    outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
   },
